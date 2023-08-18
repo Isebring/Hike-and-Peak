381eadb2e36e173b0def88b79b8810e9e8e288b1
Hampus Isebring <hampus.isebring@gmail.com>
2023-08-18 09:43:25 +0200

    added new bg color

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { navData } from './data/navbardata';
 
 function App() {
+  useEffect(() => {
+    document.body.classList.add('custom-background');
+
+    return () => {
+      document.body.classList.remove('custom-background');
+    };
+  }, []);
+
   return (
     <>
       <Navbar links={navData} />
